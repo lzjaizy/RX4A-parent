b0e26f2d9eaffdb67639950c958c6fde9ed94c90
liuzijie <liu.zijie@cesgroup.com.cn>
2019-03-28 12:10:30 +0800

    去除 cas

--- RX4A/src/main/java/com/rx/shiro/entity/SysPermission.java
+++ RX4A/src/main/java/com/rx/shiro/entity/SysPermission.java
@@ -2,7 +2,13 @@ package com.rx.shiro.entity;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.util.List;
 
 @Entity
--- RX4A/src/main/java/com/rx/shiro/entity/User.java
+++ RX4A/src/main/java/com/rx/shiro/entity/User.java
@@ -3,7 +3,14 @@ package com.rx.shiro.entity;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
