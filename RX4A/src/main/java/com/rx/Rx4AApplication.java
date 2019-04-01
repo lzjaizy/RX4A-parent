5ccbb111b8f846c0bf4d3cb5e3fc62f167604726
liuzijie <liu.zijie@cesgroup.com.cn>
2019-04-01 15:58:40 +0800

    druid 配置

--- RX4A/src/main/java/com/rx/Rx4AApplication.java
+++ RX4A/src/main/java/com/rx/Rx4AApplication.java
@@ -3,10 +3,12 @@ package com.rx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
 @SpringBootApplication
 @MapperScan("com.rx.demo.dao")
+@ServletComponentScan(value = "com.rx.config")
 //@ComponentScan(basePackages={"com"})
 public class Rx4AApplication {
 
