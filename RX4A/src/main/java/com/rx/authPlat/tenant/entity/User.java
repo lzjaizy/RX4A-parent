3a1cdacd64caee365195d868fddc2c026d4a56ba
mengqingjun1990 <337405389@qq.com>
2019-04-19 09:43:05 +0800

    websqldatabase

---
+++ RX4A/src/main/java/com/rx/authPlat/tenant/entity/User.java
@@ -0,0 +1,422 @@
+package com.rx.authPlat.tenant.entity;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * @PackageName com.rx.authPlat.tenant.entity
+ * @ClassName User
+ * @Description 用户实体类
+ * @Author MengQingJun
+ * @Date 2019/4/13
+ * @Version 2.0
+ **/
+public class User {
+
+    private String id;
+    private String name;
+    private String loginName;
+    private Integer age;
+    private String address;
+    private Integer gender;
+    private String email;
+    private String abbreviation;
+    private Date birthDay;
+    private String fax;
+    private String picUrl;
+    private String comments;
+    private String tenantId;
+    private Integer status;
+    private Integer approvalStatus;
+    private Timestamp createTime;
+    private Timestamp modifyTime;
+    private String createUser;
+    private String modifyUser;
+    private String mobile;
+    private String telephone;
+    private Long showOrder;
+    private Date beginValidTime;
+    private Date endValidTime;
+    private String identityId;
+    private Integer isSuperadmin;
+    private String unitId;
+    private String flag1;
+    private String flag2;
+    private String flag3;
+    private String flag4;
+    private String flag5;
+    private String flag6;
+    private String flag7;
+    private String flag8;
+    private String flag9;
+    private String flag10;
+    private Integer bindAddressType;
+    private String bindIpAddress;
+    private String bindMacAddress;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getApprovalStatus() {
+        return approvalStatus;
+    }
+
+    public void setApprovalStatus(Integer approvalStatus) {
+        this.approvalStatus = approvalStatus;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public Timestamp getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Timestamp modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public String getModifyUser() {
+        return modifyUser;
+    }
+
+    public void setModifyUser(String modifyUser) {
+        this.modifyUser = modifyUser;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Long getShowOrder() {
+        return showOrder;
+    }
+
+    public void setShowOrder(Long showOrder) {
+        this.showOrder = showOrder;
+    }
+
+    public Date getBeginValidTime() {
+        return beginValidTime;
+    }
+
+    public void setBeginValidTime(Date beginValidTime) {
+        this.beginValidTime = beginValidTime;
+    }
+
+    public Date getEndValidTime() {
+        return endValidTime;
+    }
+
+    public void setEndValidTime(Date endValidTime) {
+        this.endValidTime = endValidTime;
+    }
+
+    public String getIdentityId() {
+        return identityId;
+    }
+
+    public void setIdentityId(String identityId) {
+        this.identityId = identityId;
+    }
+
+    public Integer getIsSuperadmin() {
+        return isSuperadmin;
+    }
+
+    public void setIsSuperadmin(Integer isSuperadmin) {
+        this.isSuperadmin = isSuperadmin;
+    }
+
+    public String getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(String unitId) {
+        this.unitId = unitId;
+    }
+
+    public String getFlag1() {
+        return flag1;
+    }
+
+    public void setFlag1(String flag1) {
+        this.flag1 = flag1;
+    }
+
+    public String getFlag2() {
+        return flag2;
+    }
+
+    public void setFlag2(String flag2) {
+        this.flag2 = flag2;
+    }
+
+    public String getFlag3() {
+        return flag3;
+    }
+
+    public void setFlag3(String flag3) {
+        this.flag3 = flag3;
+    }
+
+    public String getFlag4() {
+        return flag4;
+    }
+
+    public void setFlag4(String flag4) {
+        this.flag4 = flag4;
+    }
+
+    public String getFlag5() {
+        return flag5;
+    }
+
+    public void setFlag5(String flag5) {
+        this.flag5 = flag5;
+    }
+
+    public String getFlag6() {
+        return flag6;
+    }
+
+    public void setFlag6(String flag6) {
+        this.flag6 = flag6;
+    }
+
+    public String getFlag7() {
+        return flag7;
+    }
+
+    public void setFlag7(String flag7) {
+        this.flag7 = flag7;
+    }
+
+    public String getFlag8() {
+        return flag8;
+    }
+
+    public void setFlag8(String flag8) {
+        this.flag8 = flag8;
+    }
+
+    public String getFlag9() {
+        return flag9;
+    }
+
+    public void setFlag9(String flag9) {
+        this.flag9 = flag9;
+    }
+
+    public String getFlag10() {
+        return flag10;
+    }
+
+    public void setFlag10(String flag10) {
+        this.flag10 = flag10;
+    }
+
+    public Integer getBindAddressType() {
+        return bindAddressType;
+    }
+
+    public void setBindAddressType(Integer bindAddressType) {
+        this.bindAddressType = bindAddressType;
+    }
+
+    public String getBindIpAddress() {
+        return bindIpAddress;
+    }
+
+    public void setBindIpAddress(String bindIpAddress) {
+        this.bindIpAddress = bindIpAddress;
+    }
+
+    public String getBindMacAddress() {
+        return bindMacAddress;
+    }
+
+    public void setBindMacAddress(String bindMacAddress) {
+        this.bindMacAddress = bindMacAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", loginName='" + loginName + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", gender=" + gender +
+                ", email='" + email + '\'' +
+                ", abbreviation='" + abbreviation + '\'' +
+                ", birthDay=" + birthDay +
+                ", fax='" + fax + '\'' +
+                ", picUrl='" + picUrl + '\'' +
+                ", comments='" + comments + '\'' +
+                ", tenantId='" + tenantId + '\'' +
+                ", status=" + status +
+                ", approvalStatus=" + approvalStatus +
+                ", createTime=" + createTime +
+                ", modifyTime=" + modifyTime +
+                ", createUser='" + createUser + '\'' +
+                ", modifyUser='" + modifyUser + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", showOrder=" + showOrder +
+                ", beginValidTime=" + beginValidTime +
+                ", endValidTime=" + endValidTime +
+                ", identityId='" + identityId + '\'' +
+                ", isSuperadmin=" + isSuperadmin +
+                ", unitId='" + unitId + '\'' +
+                ", flag1='" + flag1 + '\'' +
+                ", flag2='" + flag2 + '\'' +
+                ", flag3='" + flag3 + '\'' +
+                ", flag4='" + flag4 + '\'' +
+                ", flag5='" + flag5 + '\'' +
+                ", flag6='" + flag6 + '\'' +
+                ", flag7='" + flag7 + '\'' +
+                ", flag8='" + flag8 + '\'' +
+                ", flag9='" + flag9 + '\'' +
+                ", flag10='" + flag10 + '\'' +
+                ", bindAddressType=" + bindAddressType +
+                ", bindIpAddress='" + bindIpAddress + '\'' +
+                ", bindMacAddress='" + bindMacAddress + '\'' +
+                '}';
+    }
+}
